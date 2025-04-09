6707a29df9c40c113796239a8587275d9be8522c
Suryansh Gupta <suryanshgupta9933@gmail.com>
2025-04-10 04:32:56 +0530

    typo

--- src/api/app.py
+++ src/api/app.py
@@ -114,4 +114,7 @@ def create_app() -> FastAPI:
     return app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True) 
+    if settings.app_env == "development":
+        uvicorn.run("src.api.app:app", host="0.0.0.0", port=8000, reload=True)
+    else:
+        uvicorn.run(app, host="0.0.0.0", port=8000) 
